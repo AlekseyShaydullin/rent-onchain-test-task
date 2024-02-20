16af3c3e56c617da97517e76906c9124e81cb104
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2024-02-20 22:11:24 +0300

    fix bug build

--- src/components/modals/modal-my-menu/modal-my-menu.tsx
+++ src/components/modals/modal-my-menu/modal-my-menu.tsx
@@ -1,65 +1,67 @@
-import { FC } from 'react';
-import cn from 'classnames';
+// import { FC } from 'react';
+// import cn from 'classnames';
 
-import style from './modal-my-menu.module.scss';
+// import style from './modal-my-menu.module.scss';
 
-import ModalPopup from '../modal-popup/modal-popup';
-import Typography from '../../ui/typography/typography';
-import { optionsCard } from '../../sidebar/cardConfig';
-import Icon from '../../ui/icon/icon';
-import Card from '../../sidebar/card/card';
+// import ModalPopup from '../modal-popup/modal-popup';
+// import Card from '../../sidebar/card/card';
 
-interface IModalMyMenu {
-  closeModal: () => void;
-}
+// import Typography from '../../ui/typography/typography';
+// import Icon from '../../ui/icon/icon';
 
-const ModalMyMenu: FC<IModalMyMenu> = ({ closeModal }): JSX.Element => {
-  return (
-    <ModalPopup onClick={closeModal}>
-      <>
-        <div className={style.header}>
-          <Typography tag={'p'} extraClass={style.title} color={'white'}>
-            {'MY\u00A0MENU'}
-          </Typography>
-        </div>
-        <ul className={style.cards}>
-          {optionsCard.map((options, index) => {
-            return (
-              <li className={style.menu} key={index}>
-                <div className={style.menu__wrapper}>
-                  <Icon
-                    name={options.icon}
-                    isColored
-                    extraClass={style.menu__icon}
-                  />
-                  <Typography
-                    tag={'p'}
-                    fontFamily={'secondary'}
-                    extraClass={cn(style.menu__title)}
-                    color={'white'}
-                  >
-                    {options.title}
-                  </Typography>
-                  <Icon
-                    name={'chevron'}
-                    isColored
-                    extraClass={cn(style.menu__icon)}
-                  />
-                </div>
-                <div className={style.fields}>
-                  {options.fields.map((fields, index) => {
-                    return (
-                      <Card options={fields} key={index} isOpen={isOpen} />
-                    )
-                  })}
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </>
-    </ModalPopup>
-  );
-};
+// import { optionsCard } from '../../sidebar/cardConfig';
 
-export default ModalMyMenu;
+// interface IModalMyMenu {
+//   closeModal: () => void;
+// }
+
+// const ModalMyMenu: FC<IModalMyMenu> = ({ closeModal }): JSX.Element => {
+//   return (
+//     <ModalPopup onClick={closeModal}>
+//       <>
+//         <div className={style.header}>
+//           <Typography tag={'p'} extraClass={style.title} color={'white'}>
+//             {'MY\u00A0MENU'}
+//           </Typography>
+//         </div>
+//         <ul className={style.cards}>
+//           {optionsCard.map((options, index) => {
+//             return (
+//               <li className={style.menu} key={index}>
+//                 <div className={style.menu__wrapper}>
+//                   <Icon
+//                     name={options.icon}
+//                     isColored
+//                     extraClass={style.menu__icon}
+//                   />
+//                   <Typography
+//                     tag={'p'}
+//                     fontFamily={'secondary'}
+//                     extraClass={cn(style.menu__title)}
+//                     color={'white'}
+//                   >
+//                     {options.title}
+//                   </Typography>
+//                   <Icon
+//                     name={'chevron'}
+//                     isColored
+//                     extraClass={cn(style.menu__icon)}
+//                   />
+//                 </div>
+//                 <div className={style.fields}>
+//                   {options.fields.map((option, index) => {
+//                     return (
+//                       <Card options={fields} key={index} isOpen={isOpen} />
+//                     );
+//                   })}
+//                 </div>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </>
+//     </ModalPopup>
+//   );
+// };
+
+// export default ModalMyMenu;
